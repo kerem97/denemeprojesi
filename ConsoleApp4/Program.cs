05f37d9f146e4c3d0c56f02501fd0aec75adf0ed
KeremDurak <keremdurak@ogr.halic.edu.tr>
2021-05-10 15:08:09 +0300

    Completed

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0, sayac = 0, sayi;
+            double ort;
+            Console.Write("Bir sayı giriniz :");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            while (sayi != 0)
+            {
+                sayac++;
+                toplam += sayi;
+                Console.Write("Bir sayı giriniz :");
+                sayi = Convert.ToInt32(Console.ReadLine());
+            }
+            if (sayac != 0)
+            {
+                ort = (double)toplam / sayac;
+                Console.WriteLine("Sayıların ortalaması={0}", ort);
+            }
+            else
+            {
+                Console.WriteLine("Sayı girilmedi");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
